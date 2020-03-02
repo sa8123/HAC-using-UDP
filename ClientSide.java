eeebd501b9fe6cfe409f86ed67b34d0f1df67faf
Sudip Aryal <38017418+sa8123@users.noreply.github.com>
2020-03-01 21:50:18 -0600

    Add files via upload

---
+++ ClientSide.java
@@ -0,0 +1,63 @@
+package edu.truman.serverside;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ClientSide
+{
+	DatagramSocket socket;
+	
+	//Constructor 
+	public ClientSide()
+	{
+		
+	}
+	
+	public void sendAndListen() 
+	{
+		try 
+		{
+			Timer timer = new Timer();
+			socket = new DatagramSocket();
+			InetAddress address = InetAddress.getByName("localhost");
+			String message = "Hello! from Client";
+			byte[] sendMessage = message.getBytes();
+			byte[] receiveMessage = new byte[1024];
+			DatagramPacket sendPacket = new DatagramPacket(sendMessage, sendMessage.length, address, 9876);
+			socket.send(sendPacket);
+			System.out.println("Message sent from client");
+			DatagramPacket receivePacket = new DatagramPacket(receiveMessage, receiveMessage.length);
+			socket.receive(receivePacket);
+			String actualData = new String(receivePacket.getData());
+			System.out.println("Response from server:" + actualData);
+			//socket.close();
+			
+			
+		} 
+		catch (SocketException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (UnknownHostException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) 
+	{
+		ClientSide theClient = new ClientSide();
+		theClient.sendAndListen();
+	}
+
+}
